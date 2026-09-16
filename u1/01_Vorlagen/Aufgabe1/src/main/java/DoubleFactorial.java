39d82f821f7b84b909e46b7595e396da57b724c5
Laurin Fürer <90036326+LFanonym@users.noreply.github.com>
2026-09-16 11:33:22 +0200

    solve a1

--- u1/01_Vorlagen/Aufgabe1/src/main/java/DoubleFactorial.java
+++ u1/01_Vorlagen/Aufgabe1/src/main/java/DoubleFactorial.java
@@ -1,5 +1,24 @@
+import util.SimpleIO;
+
 public class DoubleFactorial {
 	void main() {
-        // TODO
+		int number = SimpleIO.readInt("Doppel-Fakultät für 'n': ");
+		if (number < 0) {
+			IO.println("Undefined");
+		} else if (number > 69 || (number % 2 == 0 && number > 60)) {
+			IO.println("Too large");
+		} else {
+			IO.println(doubleFactorial(number));
+		}
+	}
+
+	private long doubleFactorial(int input) {
+		long product = 1;
+		int counter = 2 - (input % 2);
+		while (counter <= input) {
+			product *= counter;
+			counter += 2;
+		}
+		return product;
 	}
 }
